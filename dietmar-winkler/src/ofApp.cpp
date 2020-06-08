0ddff474d73c7499087c6ed961e29765295fb8bf
Allie Jones <allie@alliejon.es>
2020-06-08 18:37:13 -0400

    Draw full cobol

---
+++ dietmar-winkler/src/ofApp.cpp
@@ -0,0 +1,167 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  red = ofColor(236, 78, 68);
+  blue = ofColor(0, 108, 185);
+  cyan = ofColor(0, 163, 218);
+
+  circleRadius = 75.;
+
+  letterPath.setCircleResolution(100);
+  letterPathNoL.setCircleResolution(100);
+
+  vector<ofPath> paths = ofApp::genLetterPaths(circleRadius);
+  for (int i = 0; i < paths.size(); i++) {
+    letterPath.append(paths[i]);
+    if (i != paths.size() - 1) {
+      letterPathNoL.append(paths[i]);
+    }
+  }
+
+  ofRectangle letterPathBounds = ofApp::getBoundingBoxOfPath(letterPath);
+  letterPathFbo.allocate(letterPathBounds.getWidth(),
+                         letterPathBounds.getHeight());
+  letterPathMaskFbo.allocate(letterPathBounds.getWidth(),
+                             letterPathBounds.getHeight());
+
+  letterPathFbo.begin();
+  ofClear(0, 0, 0);
+  // y translation needs to accomodate ascenders
+  ofTranslate(circleRadius, circleRadius * 4. / 3);
+  letterPath.setColor(cyan);
+  letterPath.draw();
+  letterPathFbo.end();
+
+  letterPathMaskFbo.begin();
+  ofClear(0, 0, 0);
+  // y translation needs to accomodate ascenders
+  letterPathNoL.draw(circleRadius * (5. / 3), circleRadius * (4. / 3));
+  letterPathMaskFbo.end();
+
+  letterPathFbo.getTexture().setAlphaMask(letterPathMaskFbo.getTexture());
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+  ofBackground(60, 49, 72);
+
+  letterPathNoL.setColor(red);
+  letterPathNoL.draw(circleRadius * 8. / 3, circleRadius * 2);
+
+  letterPath.setColor(blue);
+  letterPath.draw(circleRadius * 2, circleRadius * 2);
+
+  letterPathFbo.draw(circleRadius, circleRadius * 2. / 3);
+}
+
+vector<ofPath> ofApp::genLetterPaths(float circleRad) {
+  float circleRadThird = circleRad / 3.;
+
+  vector<char> letters = {'c', 'o', 'b', 'o'};
+  vector<ofPath> letterPaths;
+
+  for (int i = 0; i < letters.size(); i++) {
+    char letter = letters[i];
+    ofPath path;
+    path.setCircleResolution(100);
+
+    if (letter == 'b') {
+      ofPath circle;
+      circle.setCircleResolution(100);
+      circle.circle(0, 0, circleRad);
+
+      ofPolyline rect = ofPolyline::fromRectangle(
+          ofRectangle(-circleRad, -circleRad - circleRadThird,
+                      circleRad - circleRadThird, circleRad + circleRadThird));
+      rect.close();
+      // clipper uses integers internally, scale up for smoother curves
+      rect.scale(10, 10);
+
+      ofPolyline circ = circle.getOutline()[0];
+      circ.close();
+      circ.scale(10, 10);
+
+      ofx::Clipper clipper;
+      clipper.addPolyline(circ, ClipperLib::ptSubject);
+      clipper.addPolyline(rect, ClipperLib::ptClip);
+      ofPolyline result = clipper.getClipped(ClipperLib::ClipType::ctUnion)[0];
+      result.scale(0.1,
+                   0.1); // undo scaling for clipper
+
+      path.moveTo(result[0]);
+      for (size_t i = 1; i < result.size(); ++i) {
+        path.lineTo(result[int(i)]);
+      }
+    } else {
+      path.circle(0, 0, circleRad);
+    }
+
+    if (letter == 'c') {
+      path.rectangle(circleRadThird, -1, circleRad - circleRadThird, 2);
+    }
+
+    path.circle(0, 0, circleRadThird);
+
+    path.translate(glm::vec2(i * circleRad * 2, 0));
+
+    letterPaths.push_back(path);
+  }
+
+  ofPath pathL;
+  pathL.rectangle(0, -circleRad - circleRadThird, circleRad - circleRadThird,
+                  circleRad * 2 + circleRadThird);
+  pathL.translate(
+      glm::vec2(circleRad * 2 * (letters.size() - 1) + circleRad, 0));
+  letterPaths.push_back(pathL);
+
+  return letterPaths;
+}
+
+ofRectangle ofApp::getBoundingBoxOfPath(ofPath &path) {
+  ofRectangle rect;
+  for (int i = 0; i < path.getOutline().size(); i++) {
+    ofRectangle b = path.getOutline().at(i).getBoundingBox();
+    if (i == 0)
+      rect = b;
+    else
+      rect.growToInclude(b);
+  }
+  return rect;
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
---
+++ dietmar-winkler/src/ofApp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxClipper.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+  void setup();
+  void update();
+  void draw();
+
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y);
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+
+  vector<ofPath> genLetterPaths(float circleRad);
+  void drawLetters(ofColor fill, bool includeL = true);
+
+  ofRectangle getBoundingBoxOfPath(ofPath &path);
+
+  ofColor red;
+  ofColor blue;
+  ofColor cyan;
+
+  float circleRadius;
+  ofPath letterPath;
+  ofPath letterPathNoL;
+
+  ofFbo letterPathFbo;
+  ofFbo letterPathMaskFbo;
+};
